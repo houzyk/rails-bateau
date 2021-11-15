ef3111167d2f172b6ce17386cdc3ae703fc7c8f0
houzairkoussa <houzairmk@icloud.com>
2021-11-15 16:17:48 +0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,10 @@ Rails.application.routes.draw do
   resources :chapters, only: %i[destroy edit update]
   resources :materials, only: %i[destroy edit update]
 
-  resources :chatrooms do
+  resources :chatrooms, only: %i[index show] do
     resources :messages, only: :create
   end
 
-  resources :schedules do
-    resources :calendars
-  end
+  resources :schedules
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
