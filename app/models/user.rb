3362309ff64836185829c65109fe34897e1c83d8
Houzair Koussa <88334281+houzyk@users.noreply.github.com>
2021-11-15 13:38:58 +0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :chatrooms, through: :messages
   has_many :calendars, dependent: :destroy
-  has_many :schedules, through: :calendars
+  has_many :schedules, through: :calendars, dependent: :destroy
 end
