d0ba67ed25b71cbdd41e2d6345c93ab0338c657d
Christian Bongard <christian.bongard90@gmail.com>
2021-11-15 13:50:05 +0400

    added category reeference to subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_094448) do
+ActiveRecord::Schema.define(version: 2021_11_15_094928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2021_11_15_094448) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["schedule_id"], name: "index_calendars_on_schedule_id"
     t.index ["user_id"], name: "index_calendars_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "chatrooms", force: :cascade do |t|
@@ -54,6 +60,8 @@ ActiveRecord::Schema.define(version: 2021_11_15_094448) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_subjects_on_category_id"
     t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
@@ -76,5 +84,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_094448) do
   add_foreign_key "calendars", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "subjects", "categories"
   add_foreign_key "subjects", "users"
 end
