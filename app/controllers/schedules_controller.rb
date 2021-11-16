b46c80e1fcd1fd6ca16ef97db3771b12fb20d2db
houzairkoussa <houzairmk@icloud.com>
2021-11-16 11:58:00 +0400

    initial completion of schedules controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,15 +1,22 @@
 class SchedulesController < ApplicationController
 
   def index
-
+    @schedules = policy_scope(Schedule)
   end
 
   def new
-
+    @schedule = Schedule.new
+    authorize @schedule
   end
 
   def create
-
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to schedules_path
+    else
+      render :new
+    end
+    authorize @schedule
   end
 
   def edit
@@ -26,8 +33,8 @@ class SchedulesController < ApplicationController
 
   private
 
-  # def schedule_params
-
-  # end
+  def schedule_params
+    params.require(:schedule).permit(:content, :date)
+  end
 
 end
