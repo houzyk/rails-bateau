7685de2e0022e4afd48433069dc6ca208add90d8
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-11-16 11:24:37 +0400

    created method new

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,4 +2,8 @@ class ChaptersController < ApplicationController
   def show
     @chapter = Chapter.find(params[:id])
   end
+
+  def new
+    @chapter = Chapter.new
+  end
 end
--- app/policies/chapter_policy.rb
+++ app/policies/chapter_policy.rb
@@ -4,4 +4,8 @@ class ChapterPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
 end
