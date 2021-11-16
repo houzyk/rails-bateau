5284666e576a18982aa6f58bf25f30d8619e6efb
houzairkoussa <houzairmk@icloud.com>
2021-11-16 11:00:38 +0400

    merging controllers for messages and chatroons

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,10 +2,10 @@ class ChatroomsController < ApplicationController
   def index
     @restaurants = policy_scope(Chatroom)
   end
-  
+
   def show
+    @message = Message.new
     @chatroom = Chatroom.find(params[:id])
     authorize @chatroom
   end
-
 end
