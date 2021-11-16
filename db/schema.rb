76fbb1664fc66c7465f09cf09f8115771f1a2f0e
houzairkoussa <houzairmk@icloud.com>
2021-11-16 15:36:49 +0400

    removed calendars table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_124259) do
+ActiveRecord::Schema.define(version: 2021_11_16_113449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,15 +41,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_124259) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "calendars", force: :cascade do |t|
-    t.bigint "schedule_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["schedule_id"], name: "index_calendars_on_schedule_id"
-    t.index ["user_id"], name: "index_calendars_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -134,8 +125,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_124259) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "calendars", "schedules"
-  add_foreign_key "calendars", "users"
   add_foreign_key "chapters", "subjects"
   add_foreign_key "classrooms", "subjects"
   add_foreign_key "classrooms", "users"
