31c767df74b9614db6de194cb88578c1a9de7e37
houzairkoussa <houzairmk@icloud.com>
2021-11-16 19:33:15 +0400

    refactored materials controller

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,50 +1,35 @@
 class MaterialsController < ApplicationController
-  # after_action :authorize_material
-  # def show
-  #   @material = Material.find(params[:material_id])
-  #   authorize @material
-  # end
+  after_action :authorize_material
+  before_action :set_material, only: %i[edit update destroy]
+  before_action :set_subject_chapter, only: %i[new create]
 
   def new
-    @subject = Subject.find(params[:subject_id])
-    @chapter = Chapter.find(params[:chapter_id])
     @material = Material.new
-    authorize @material
   end
 
   def create
     @material = Material.new(material_params)
-    @subject = Subject.find(params[:subject_id])
-    @chapter = Chapter.find(params[:chapter_id])
     @material.chapter = @chapter
     if @material.save
       redirect_to subject_chapter_path(@subject, @chapter)
     else
       render :new
     end
-    authorize @material
   end
 
-  def edit
-    @material = Material.find(params[:id])
-    authorize @material
-  end
+  def edit; end
 
   def update
-    @material = Material.find(params[:id])
     if @material.update(material_params)
       redirect_to subject_chapter_path(@material.chapter.subject, @material.chapter)
     else
       render :edit
     end
-    authorize @material
   end
 
   def destroy
-    @material = Material.find(params[:id])
     # ! INSERT AN IF TO CHECK FOR POTENTIAL ERROS
     redirect_to subject_chapter_path(@material.chapter.subject, @material.chapter) if @material.destroy
-    authorize @material
   end
 
   private
@@ -53,7 +38,16 @@ class MaterialsController < ApplicationController
     params.require(:material).permit(:name, :subject_id, :chapter_id)
   end
 
-  # def authorize_material
-  #   authorize @material
-  # end
+  def authorize_material
+    authorize @material
+  end
+
+  def set_material
+    @material = Material.find(params[:id])
+  end
+
+  def set_subject_chapter
+    @subject = Subject.find(params[:subject_id])
+    @chapter = Chapter.find(params[:chapter_id])
+  end
 end
