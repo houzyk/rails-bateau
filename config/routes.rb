cee22ac91815be8166503db40dc866c2f4d0158b
houzairkoussa <houzairmk@icloud.com>
2021-11-16 14:11:31 +0400

    policy for classroom created

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,3 @@ Rails.application.routes.draw do
   resources :schedules, except: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-# ! MISSING CONTROLLER
-# category
