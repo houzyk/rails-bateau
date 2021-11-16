8a949a65a768e7979a7f9b074ed999a54fa932a7
houzairkoussa <houzairmk@icloud.com>
2021-11-16 11:58:18 +0400

    added pundit to schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -20,15 +20,21 @@ class SchedulesController < ApplicationController
   end
 
   def edit
-
+    @schedule = Schedule.find(params[:id])
+    authorize @schedule
   end
 
   def update
-
+    @schedule = Schedule.find(params[:id])
+    @schedule.update(schedule_params)
+    redirect_to schedules_path
+    authorize @schedule
   end
 
   def destroy
-
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    authorize @schedule
   end
 
   private
