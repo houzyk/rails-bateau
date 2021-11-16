242a55268d61ec388299a4c06f6cc5341290b426
Christian Bongard <christian.bongard90@gmail.com>
2021-11-16 11:52:43 +0400

    added materials controller and policy

---
+++ app/policies/material_policy.rb
@@ -0,0 +1,15 @@
+class MaterialPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def index?
+    true
+  end
+end
