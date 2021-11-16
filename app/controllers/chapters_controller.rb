8cc1d88cefb302ca6e324f3c589037acea2e7df1
houzairkoussa <houzairmk@icloud.com>
2021-11-16 16:47:07 +0400

    methods and policy for chapters created

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,9 +1,35 @@
 class ChaptersController < ApplicationController
+  def index
+
+  end
+
   def show
     @chapter = Chapter.find(params[:id])
   end
 
   def new
     @chapter = Chapter.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def chapter_params
+
   end
 end
--- app/policies/chapter_policy.rb
+++ app/policies/chapter_policy.rb
@@ -5,7 +5,23 @@ class ChapterPolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    true
+  end
+
   def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
     true
   end
 end
