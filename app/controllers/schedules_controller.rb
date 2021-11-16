b2cc3d6a08bbafceeaf314e4f3033b9346390742
houzairkoussa <houzairmk@icloud.com>
2021-11-16 11:42:11 +0400

    generated schedule controller and pundint access

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,33 @@
+class SchedulesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  # def schedule_params
+
+  # end
+
+end
---
+++ app/policies/schedule_policy.rb
@@ -0,0 +1,23 @@
+class SchedulePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end
