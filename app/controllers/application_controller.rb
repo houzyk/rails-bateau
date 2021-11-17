cdf94249e58bd68908c65c5601a3fa40ab4c160e
houzairkoussa <houzairmk@icloud.com>
2021-11-17 11:01:18 +0400

    authorized personalised user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
+
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
 
   private
 
