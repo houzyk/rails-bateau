3c791682e2aade5c22a707d55695beabfcf7c1de
Christian Bongard <christian.bongard90@gmail.com>
2021-11-17 17:06:35 +0400

    added content field to materials and updated the show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_114010) do
+ActiveRecord::Schema.define(version: 2021_11_17_125607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_114010) do
     t.bigint "chapter_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content"
     t.index ["chapter_id"], name: "index_materials_on_chapter_id"
   end
 
