6762e9590a6bd020675586c3f7789488c6cdf31c
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-11-17 16:06:10 +0400

    completed dashboard-page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @user = current_user
+    @subjects = Subject.all
   end
 end
