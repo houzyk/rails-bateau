1d9f6495049de98ede52ec8aaa00f44914131ae1
dhanishtha goordoyal <dhanisthag@gmail.com>
2021-11-18 18:18:13 +0400

    content added to search schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,4 +6,12 @@ class Schedule < ApplicationRecord
   def date_cannot_be_in_the_past
     errors.add(:date, 'can\'t be in the past') if date.present? && date < Date.today
   end
+
+include PgSearch::Model
+
+  pg_search_scope :search_by_content,
+    against: [:content],
+    using: {
+    tsearch: { prefix: true }
+    }
 end
