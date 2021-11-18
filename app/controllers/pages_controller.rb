2476ede4fe1e37cee3315ca7adc117363fe83743
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-11-18 16:23:41 +0400

    completed PG-Search functionality for campus page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,18 +7,11 @@ class PagesController < ApplicationController
   end
 
   def campus
-    @users = User.except(@current_user)
-    if params[:query].present?
-      @users = @users.campus_search(params[:query])
-      @students = @users.where(teacher: false)
-      @count_students = @students.count
-      @teachers = @users.where(teacher: true)
-      @count_teachers = @teachers.count
-    else
-      @students = @users.where(teacher: false)
-      @count_students = @students.count
-      @teachers = @users.where(teacher: true)
-      @count_teachers = @teachers.count
-    end
+    @users = User.all
+    @users = @users.campus_search(params[:query]) if params[:query].present?
+    @students = @users.where(teacher: false)
+    @teachers = @users.where(teacher: true)
+    @count_students = @students.count
+    @count_teachers = @teachers.count
   end
 end
