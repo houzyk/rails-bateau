9ff163cade03f5ec3914cb16603f26533c76563a
Christian Bongard <christian.bongard90@gmail.com>
2021-11-19 07:52:26 +0400

    added seed for category photos

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,7 +1,7 @@
 class Chatroom < ApplicationRecord
   has_many :participants, dependent: :destroy
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_many :users, through: :messages
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
