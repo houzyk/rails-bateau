612166bf773f2a313e614bd7f2d85adb2d40efe9
houzairkoussa <houzairmk@icloud.com>
2021-11-22 09:45:59 +0400

    initialized js for twillo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,22 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("trix")
+require("@rails/actiontext")
+// ! TWILLO
+require("jquery")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap";
+import "trix";
+import "@rails/actiontext";
+import { initChatroomCable } from '../channels/chatroom_channel';
+// ! TWILLO
+import $ from 'jquery';
+window.jQuery = $; window.$ = $;
 
 Rails.start()
 Turbolinks.start()
@@ -19,16 +30,10 @@ ActiveStorage.start()
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
-import "trix";
-import "@rails/actiontext";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { initChatroomCable } from '../channels/chatroom_channel';
 
-require("trix")
-require("@rails/actiontext")
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
