d193a3c7ac82ee4ae13e45415ddfdb3c2b6e7664
houzairkoussa <houzairmk@icloud.com>
2021-11-22 09:55:55 +0400

    added room controller and policy

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,17 @@
+class RoomsController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
---
+++ app/policies/room_policy.rb
@@ -0,0 +1,19 @@
+class RoomPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end
