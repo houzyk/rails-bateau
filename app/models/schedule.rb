01e94b2d3e1f7529d1eb5f516ef93b03d8a9c937
dhanishtha goordoyal <dhanisthag@gmail.com>
2021-11-25 16:42:52 +0400

    added rich text to model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,6 +2,7 @@ class Schedule < ApplicationRecord
   belongs_to :user
   validates :content, :date, presence: true
   validate :date_cannot_be_in_the_past
+  has_rich_text :content
 
   def date_cannot_be_in_the_past
     errors.add(:date, 'can\'t be in the past') if date.present? && date < Date.today
